41596de25f7cd556e73389d843da0dee686a01d2
Oleksii Fedorenko <fedorenkolesha@gmail.com>
2023-04-12 23:55:51 +0300

    Add prop-types and draft files

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return;
+  }
+}
